a083c600cd5fe077d355c8ce91a2e0028e8c4ac5
Edwin nduti <52169617+edwinnduti@users.noreply.github.com>
2020-07-22 17:55:46 +0300

    added go file

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"time"
+	"log"
+	"html/template"
+	"github.com/gorilla/mux"
+)
+
+const(
+	PORT = "6075"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func main(){
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/",welcomeHandler).Methods("GET")
+	r.HandleFunc("/about",aboutHandler).Methods("GET")
+//	r.HandleFunc("/{name}",nameHandler).Methods("GET")
+	r.HandleFunc("/home",homeHandler).Methods("GET")
+
+
+	server := &http.Server{
+		Addr: ":"+PORT,
+		Handler :r,
+	}
+	log.Printf("Listening on port %s...",PORT)
+	server.ListenAndServe()
+}
+
+func welcomeHandler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"Welcome Mr Edwin")
+}
+
+func aboutHandler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"<h1 align=\"center\">MY SITE</h1>")
+}
+
+func nameHandler(w http.ResponseWriter,r *http.Request){
+	vars := mux.Vars(r)
+	name := vars["name"]
+	fmt.Fprintf(w,"<h1 align=\"center\">WELCOME MR. %s TO YOUR SITE.<br> YOU VISITED THIS SITE ON %s</h1>",name,time.Now())
+}
+
+func homeHandler(w http.ResponseWriter,r *http.Request){
+	err := templates.ExecuteTemplate(w,"base",nil)
+	if err != nil{
+		log.Println(err)
+	}
+}
